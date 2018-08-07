381260cefa29aa1bf63af966068f0780a8ad5013
William Zhou <williamkzhou@hotmail.com>
2018-08-07 20:39:12 +0100

    Add files via upload

---
+++ src/main/java/org/masterwill/tollstation/com/tollstationsystemconfigserver/TollStationSystemConfigServerApplication.java
@@ -0,0 +1,12 @@
+package org.masterwill.tollstation.com.tollstationsystemconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollStationSystemConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollStationSystemConfigServerApplication.class, args);
+	}
+}
---
+++ src/test/java/org/masterwill/tollstation/com/tollstationsystemconfigserver/TollStationSystemConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.masterwill.tollstation.com.tollstationsystemconfigserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TollStationSystemConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
