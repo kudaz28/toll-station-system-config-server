b545d28f8a4b089f4c05001a4ae05a48a3f1810c
William Zhou <williamkzhou@hotmail.com>
2018-08-07 21:18:44 +0100

    Updated config files.

---
+++ src/main/java/org/tollstation/com/TollStationSystemConfigServerApplication.java
@@ -0,0 +1,12 @@
+package org.tollstation.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollStationSystemConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollStationSystemConfigServerApplication.class, args);
+	}
+}
