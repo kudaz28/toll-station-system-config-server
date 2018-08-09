9f494da495aab8ee377574f85193734ff002f670
William Zhou <williamkzhou@hotmail.com>
2018-08-09 16:45:34 +0100

    Updated project dependencies

--- src/main/java/org/masterwill/tollstation/configserver/TollStationSystemConfigServerApplication.java
+++ src/main/java/org/masterwill/tollstation/configserver/TollStationSystemConfigServerApplication.java
@@ -1,9 +1,11 @@
-package org.masterwill.tollstation.com.tollstationsystemconfigserver;
+package org.masterwill.tollstation.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class TollStationSystemConfigServerApplication {
 
 	public static void main(String[] args) {
